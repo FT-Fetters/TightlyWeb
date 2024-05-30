6ead70283963d0355319a48757f8f2b0107c1915
FT-Fetters <1438255191@qq.com>
2024-05-30 15:04:48 +0800

    Test #2

---
+++ src/test/java/entity/PostTestEntity.java
@@ -0,0 +1,10 @@
+ package entity;
+
+import lombok.Data;
+
+@Data
+public class PostTestEntity {
+
+    private Integer a;
+
+}
--- src/test/java/ioc/cat/WebCat.java
+++ src/test/java/ioc/cat/WebCat.java
@@ -1,9 +1,11 @@
 package ioc.cat;
 
+import com.heybcat.tightlyweb.http.annotation.Body;
 import com.heybcat.tightlyweb.http.annotation.WebEndpoint;
 import com.heybcat.tightlyweb.http.annotation.WebMapping;
 import com.heybcat.tightlyweb.ioc.annotation.Cat;
 import com.heybcat.tightlyweb.ioc.annotation.Inject;
+import entity.PostTestEntity;
 import xyz.ldqc.tightcall.protocol.http.HttpMethodEnum;
 
 @Cat
@@ -20,7 +22,13 @@ public class WebCat {
 
     @WebMapping(value = "/get", method = HttpMethodEnum.GET)
     public String get(int a){
+        // ioc注入
         String eat = injectTestObject.getBlackCat().eat();
         return "a = " + a + " eat : " + eat;
+    }
+
+    @WebMapping(value = "/post", method = HttpMethodEnum.POST)
+    public String post(@Body PostTestEntity entity){
+        return "post body: " + entity.getA();
     }
 }
