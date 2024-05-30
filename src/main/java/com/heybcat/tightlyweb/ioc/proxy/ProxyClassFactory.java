3ab428901f81eb0c33ba54dc5e1e6f229702d396
FT-Fetters <1438255191@qq.com>
2024-05-30 11:46:47 +0800

    Fixed proxy class can not get target method's parameter true name

--- src/main/java/com/heybcat/tightlyweb/ioc/proxy/ProxyClassFactory.java
+++ src/main/java/com/heybcat/tightlyweb/ioc/proxy/ProxyClassFactory.java
@@ -3,7 +3,10 @@ package com.heybcat.tightlyweb.ioc.proxy;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.type.TypeDefinition;
 import net.bytebuddy.dynamic.DynamicType.Builder;
+import net.bytebuddy.dynamic.DynamicType.Builder.MethodDefinition.ParameterDefinition.Annotatable;
+import net.bytebuddy.dynamic.DynamicType.Builder.MethodDefinition.ParameterDefinition.Initial;
 import net.bytebuddy.implementation.MethodDelegation;
 import xyz.ldqc.tightcall.consumer.proxy.factory.ProxySupport;
 import xyz.ldqc.tightcall.provider.service.ByteBuddyToCglibAdapter;
@@ -28,11 +31,24 @@ public class ProxyClassFactory {
             proxySupport.getInterceptor());
 
         for (Method declaredMethod : proxySupport.getTargetClass().getDeclaredMethods()) {
-            builder = builder.defineMethod(declaredMethod.getName(), declaredMethod.getReturnType(),
-                    declaredMethod.getModifiers())
-                .withParameters(declaredMethod.getParameterTypes())
-                .intercept(MethodDelegation.to(adapter))
-                .annotateMethod(declaredMethod.getAnnotations());
+
+            Initial<?> defineMethod = builder.defineMethod(declaredMethod.getName(),
+                declaredMethod.getReturnType(),
+                declaredMethod.getModifiers());
+            if (declaredMethod.getParameterTypes().length > 0) {
+                for (int i = 0; i < declaredMethod.getParameterTypes().length; i++) {
+                    Annotatable<?> annotatable = defineMethod.withParameter(
+                        declaredMethod.getParameterTypes()[i],
+                        declaredMethod.getParameters()[i].getName(), i);
+                    annotatable.annotateParameter(declaredMethod.getParameterAnnotations()[i]);
+                    builder = annotatable.intercept(MethodDelegation.to(adapter))
+                        .annotateMethod(declaredMethod.getAnnotations());
+                }
+            }else{
+                builder = defineMethod.intercept(MethodDelegation.to(adapter))
+                    .annotateMethod(declaredMethod.getAnnotations());
+            }
+
         }
         return builder
             .make()
