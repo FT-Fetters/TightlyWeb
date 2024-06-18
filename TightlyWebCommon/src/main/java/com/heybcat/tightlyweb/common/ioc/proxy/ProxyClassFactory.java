e4367e432b684bf0f0b2be2af1034081e776b9fb
FT-Fetters <1438255191@qq.com>
2024-06-18 10:53:30 +0800

    Fixed parameter wrong modifiers

--- TightlyWebCommon/src/main/java/com/heybcat/tightlyweb/common/ioc/proxy/ProxyClassFactory.java
+++ TightlyWebCommon/src/main/java/com/heybcat/tightlyweb/common/ioc/proxy/ProxyClassFactory.java
@@ -2,6 +2,7 @@ package com.heybcat.tightlyweb.common.ioc.proxy;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Objects;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.DynamicType.Builder;
 import net.bytebuddy.dynamic.DynamicType.Builder.MethodDefinition.ParameterDefinition.Annotatable;
@@ -22,37 +23,40 @@ public class ProxyClassFactory {
     public static Class<?> doByteBuddyProxy(ProxySupport proxySupport) {
         ByteBuddy byteBuddy = new ByteBuddy();
         Builder<?> builder = byteBuddy.subclass(proxySupport.getTargetClass());
+
         // 复制类注解
         for (Annotation annotation : proxySupport.getTargetClass().getAnnotations()) {
             builder = builder.annotateType(annotation);
         }
+
         ByteBuddyToCglibAdapter adapter = new ByteBuddyToCglibAdapter(
             proxySupport.getInterceptor());
 
         for (Method declaredMethod : proxySupport.getTargetClass().getDeclaredMethods()) {
-
             Initial<?> defineMethod = builder.defineMethod(declaredMethod.getName(),
                 declaredMethod.getReturnType(),
                 declaredMethod.getModifiers());
+
             if (declaredMethod.getParameterTypes().length > 0) {
+                Annotatable<?> annotatable = null;
                 for (int i = 0; i < declaredMethod.getParameterTypes().length; i++) {
-                    Annotatable<?> annotatable = defineMethod.withParameter(
-                        declaredMethod.getParameterTypes()[i],
-                        declaredMethod.getParameters()[i].getName(), i);
-                    annotatable = annotatable.annotateParameter(declaredMethod.getParameterAnnotations()[i]);
-                    builder = annotatable.intercept(MethodDelegation.to(adapter))
-                        .annotateMethod(declaredMethod.getAnnotations());
+                    annotatable = Objects.requireNonNullElse(annotatable, defineMethod)
+                        .withParameter(declaredMethod.getParameterTypes()[i],
+                            declaredMethod.getParameters()[i].getName());
+                    annotatable = annotatable.annotateParameter(
+                        declaredMethod.getParameterAnnotations()[i]);
                 }
-            }else{
+                builder = annotatable.intercept(MethodDelegation.to(adapter))
+                    .annotateMethod(declaredMethod.getAnnotations());
+            } else {
                 builder = defineMethod.intercept(MethodDelegation.to(adapter))
                     .annotateMethod(declaredMethod.getAnnotations());
             }
-
         }
+
         return builder
             .make()
             .load(ProxyClassFactory.class.getClassLoader())
             .getLoaded();
     }
-
 }
