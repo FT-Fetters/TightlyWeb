b6d9848039cb8c5e99d4128312020be63b9c8f6e
FT-Fetters <1438255191@qq.com>
2024-08-27 09:32:42 +0800

    feat: Page entity added convert function

--- TightlyWebData/src/main/java/com/heybcat/tightlyweb/sql/entity/Page.java
+++ TightlyWebData/src/main/java/com/heybcat/tightlyweb/sql/entity/Page.java
@@ -1,6 +1,7 @@
 package com.heybcat.tightlyweb.sql.entity;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 
 /**
@@ -11,9 +12,9 @@ public class Page<T> {
 
     private int total;
 
-    private int size;
+    private final int size;
 
-    private int current;
+    private final int current;
 
     private List<T> records;
 
@@ -27,6 +28,24 @@ public class Page<T> {
     public Page(int size, int current){
         this.size = size;
         this.current = current;
+    }
+
+    @FunctionalInterface
+    public interface ConvertFunction<T, M>{
+
+        /**
+         * Covert T to M
+         * @param t the object to convert
+         * @return the converted object
+         */
+        M convert(T t);
+    }
+
+    public <M> Page<M> convert(ConvertFunction<T, M> convertFunction){
+        List<M> convertedRecords = this.records.stream()
+            .map(convertFunction::convert)
+            .collect(Collectors.toList());
+        return new Page<>(size, current, convertedRecords, total);
     }
 
 
